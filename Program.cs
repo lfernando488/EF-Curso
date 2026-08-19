6b4c9583001bd939d37e28fefcaab24b6d7359d2
Luiz Fernando <lfernando488@gmail.com>
2026-08-18 22:14:55 -0300

    Consultando dados

--- Program.cs
+++ Program.cs
@@ -1,113 +1,149 @@
 ﻿using EF_Curso.Data;
-using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
 using EF_Curso.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
-    var configuration = new ConfigurationBuilder()
-    .SetBasePath(AppContext.BaseDirectory)
-    .AddJsonFile("appsettings.Local.json", optional: false)
-    .Build();
-
-    string? connectionString =
-        configuration.GetConnectionString("DefaultConnection");
-
-    if (string.IsNullOrWhiteSpace(connectionString))
-    {
-        throw new InvalidOperationException(
-            "Connection string não encontrada.");
-    }
-
-    /*
-    using var context = new ApplicationContext(connectionString);
-    var existeAtualizacao = context.Database.GetPendingMigrations();
-
-    if(!existeAtualizacao.IsNullOrEmpty())
-    {
-        //valida se executa ou nao as migrations
-    }*/
-
-    //Inserindo dados na inicializacao
-    /*var produto = new Produto
-    {
-        Descricao = "Produto para teste",
-        CodigoBarras = "134135413513",
-        Valor = 10m,
-        TipoProduto = TipoProduto.MercadoriaParaRevenda,
-        Ativo = true
-    };*/
-
-    try
-    {
-        //using var produtoContext = new ApplicationContext(connectionString);
-        //produtoContext.produtos.Add(produto);
-        //produtoContext.Set<Produto>().Add(produto);
-        //produtoContext.Entry(produto).State = EntityState.Added;
-        //produtoContext.Add(produto);
-        //var registros = produtoContext.SaveChanges(); //aplica as alteracoes no banco
-        //Console.WriteLine(string.Format("{0} registros inseridos !", registros));
-    
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(string.Format("Erro ao inserir registro(s) {0}.", ex.Message));
-    }
-
-
-//Inserindo varios itens individuais
-/*
-var produto = new Produto
+namespace EF_Curso
 {
-    Descricao = "Produto para teste",
-    CodigoBarras = "134135413513",
-    Valor = 10m,
-    TipoProduto = TipoProduto.MercadoriaParaRevenda,
-    Ativo = true
-};
-
-var cliente = new Cliente
-{
-    Nome = "Fulano teste",
-    CEP = "00000000",
-    Cidade = "São Paulo",
-    Estado = "SP",
-    Telefone = "11912345678",
-    Email = "email@teste.com.br"
-};
-
-using var context = new ApplicationContext(connectionString);
-context.AddRange(produto, cliente);
-var registros = context.SaveChanges();
-Console.WriteLine($"Registros: {registros}");
-*/
-
-//Inserindo em lista
-var listaClientes = new[]
-{
-    new Cliente
+    internal class Program
     {
-        Nome = "Fulano teste1",
-        CEP = "00000000",
-        Cidade = "São Paulo",
-        Estado = "SP",
-        Telefone = "11912345678",
-        Email = "email@teste.com.br"
-    },
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.Local.json", optional: false)
+                .Build();
 
-    new Cliente
-    {
-        Nome = "Ciclano teste",
-        CEP = "11111111",
-        Cidade = "Rio de Janeiro",
-        Estado = "RJ",
-        Telefone = "11987654321",
-        Email = "outroemail@teste.com.br"
+            string? connectionString =
+                configuration.GetConnectionString("DefaultConnection");
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException(
+                    "Connection string não encontrada.");
+            }
+
+            /*
+            using var context = new ApplicationContext(connectionString);
+            var existeAtualizacao = context.Database.GetPendingMigrations();
+
+            if(!existeAtualizacao.IsNullOrEmpty())
+            {
+                //valida se executa ou nao as migrations
+            }*/
+
+            //Inserindo dados na inicializacao
+            /*var produto = new Produto
+            {
+                Descricao = "Produto para teste",
+                CodigoBarras = "134135413513",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };*/
+
+            try
+            {
+                //using var produtoContext = new ApplicationContext(connectionString);
+                //produtoContext.produtos.Add(produto);
+                //produtoContext.Set<Produto>().Add(produto);
+                //produtoContext.Entry(produto).State = EntityState.Added;
+                //produtoContext.Add(produto);
+                //var registros = produtoContext.SaveChanges(); //aplica as alteracoes no banco
+                //Console.WriteLine(string.Format("{0} registros inseridos !", registros));
+                //InserirDados(connectionString);
+                ConsultarDados(connectionString);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Erro {0}.", ex.Message));
+            }
+
+        }
+
+        private static void InserirDados(string connection)
+        {
+            //Inserindo varios itens individuais
+            /*
+            var produto = new Produto
+            {
+                Descricao = "Produto para teste",
+                CodigoBarras = "134135413513",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Fulano teste",
+                CEP = "00000000",
+                Cidade = "São Paulo",
+                Estado = "SP",
+                Telefone = "11912345678",
+                Email = "email@teste.com.br"
+            };
+
+            using var context = new ApplicationContext(connectionString);
+            context.AddRange(produto, cliente);
+            var registros = context.SaveChanges();
+            Console.WriteLine($"Registros: {registros}");
+            */
+
+            //Inserindo em lista
+            var listaClientes = new[]
+            {
+                new Cliente
+                {
+                    Nome = "Fulano teste1",
+                    CEP = "00000000",
+                    Cidade = "São Paulo",
+                    Estado = "SP",
+                    Telefone = "11912345678",
+                    Email = "email@teste.com.br"
+                },
+
+                new Cliente
+                {
+                    Nome = "Ciclano teste",
+                    CEP = "11111111",
+                    Cidade = "Rio de Janeiro",
+                    Estado = "RJ",
+                    Telefone = "11987654321",
+                    Email = "outroemail@teste.com.br"
+                }
+            };
+
+            using var context = new ApplicationContext(connection);
+            context.AddRange(listaClientes);
+            var registros = context.SaveChanges();
+            Console.WriteLine($"Registros: {registros}");
+        }
+
+        private static void ConsultarDados(string connection)
+        {
+            using var context = new ApplicationContext(connection);
+            //var consultaViaSintaxe = (from c in context.clientes where c.Id > 0 select c).ToList();
+            //var consultaViaMetodo = context.clientes.Where(c => c.Id > 0 ).ToList(); //rastreia objetos em memoria
+            var consultaViaMetodo = context.clientes.AsNoTracking()
+                .Where(c => c.Id > 0)
+                .OrderBy(c => c.Id)
+                .ToList(); //Não rastreia objetos em memoria
+
+            foreach (var cliente in consultaViaMetodo)
+            {
+                //Busca em memoria, se nao encontrar, busca na base de dados
+                Console.WriteLine($"Consultando cliente: {cliente.Id}");
+                //context.clientes.Find(cliente.Id); //Apenas o Find() consulta a memoria, os demais consultam o banco de dados
+                context.clientes.FirstOrDefault(c => c.Id == cliente.Id);
+            }
+        }
+
     }
-};
+}
 
-using var context = new ApplicationContext(connectionString);
-context.AddRange(listaClientes);
-var registros = context.SaveChanges();
-Console.WriteLine($"Registros: {registros}");
 
 
 
